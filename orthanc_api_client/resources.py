82f5e3daeb74d7b94ac0fb7e43468e1e6cbc8458
Alain Mazy <alain@mazy.be>
2021-11-18 11:49:38 +0100

    added delete

--- orthanc_api_client/resources.py
+++ orthanc_api_client/resources.py
@@ -1,6 +1,12 @@
 import requests
+import logging
+
 from typing import List
 from .http_client import HttpClient
+
+
+logger = logging.getLogger('api-client')
+
 
 class Resources:
 
@@ -11,3 +17,13 @@ class Resources:
     def get_all_ids(self) -> List[str]:
         return self._http_client.get_json(f"/{self._url_segment}/")
 
+    def delete_all(self) -> List[str]:
+        all_ids = self.get_all_ids()
+        deleted_ids = []
+
+        for id in all_ids:
+            logger.info(f"deleting {self._url_segment} {id}")
+            self._http_client.delete(f"/{self._url_segment}/{id}")
+            deleted_ids.append(id)
+        
+        return deleted_ids
--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -17,32 +17,39 @@ class TestApiClient(unittest.TestCase):
         subprocess.run(["docker-compose", "down", "-v"], cwd=here/"docker-setup")
         subprocess.run(["docker-compose", "up", "-d"], cwd=here/"docker-setup")
 
-        cls.orthanc_a = OrthancApiClient('http://localhost:10042', user='test', pwd='test')
-        cls.orthanc_a.wait_started()
+        cls.oa = OrthancApiClient('http://localhost:10042', user='test', pwd='test')
+        cls.oa.wait_started()
 
     @classmethod
     def tearDownClass(cls):
         subprocess.run(["docker-compose", "down", "-v"], cwd=here/"docker-setup")
 
     def test_is_alive(self):
-        self.assertTrue(self.orthanc_a.is_alive())
+        self.assertTrue(self.oa.is_alive())
 
-    def test_upload_valid_dicom(self):
-        instances_ids = self.orthanc_a.upload_file( here / "stimuli/CT_small.dcm")
+    def test_upload_valid_dicom_and_delete(self):
+        self.oa.delete_all_content()
+        self.assertEqual(0, len(self.oa.studies.get_all_ids()))
+
+        instances_ids = self.oa.upload_file( here / "stimuli/CT_small.dcm")
 
         self.assertEqual('f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af', instances_ids[0])
+        self.assertEqual(1, len(self.oa.studies.get_all_ids()))
+
+        self.oa.delete_all_content()
+        self.assertEqual(0, len(self.oa.studies.get_all_ids()))
 
     def test_upload_invalid_file(self):
         with self.assertRaises(api_exceptions.BadFileFormat):
-            self.orthanc_a.upload_file( here / "__init__.py")   # __init__.py is not a valid DICOM file :-)
+            self.oa.upload_file( here / "__init__.py")   # __init__.py is not a valid DICOM file :-)
 
     def test_upload_valid_zip(self):
-        instances_ids = self.orthanc_a.upload_file( here / "stimuli/CT_small.zip")
+        instances_ids = self.oa.upload_file( here / "stimuli/CT_small.zip")
 
         self.assertEqual('f689ddd2-662f8fe1-8b18180d-ec2a2cee-937917af', instances_ids[0])
 
     def test_upload_folder(self):
-        instances_ids = self.orthanc_a.upload_folder( here / "stimuli", skip_extensions = ['.zip'])
+        instances_ids = self.oa.upload_folder( here / "stimuli", skip_extensions = ['.zip'])
 
         self.assertGreaterEqual(1, len(instances_ids))
 
