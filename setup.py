f6a196d1ed715c4b6dd5b4d0fc036ca0d2c0f991
Alain Mazy <alain@mazy.be>
2021-12-21 09:00:31 +0100

    fix tests

--- setup.py
+++ setup.py
@@ -144,8 +144,8 @@ setup(
     # Similar to `install_requires` above, these must be valid existing
     # projects.
     extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'dev': ['check-manifest', 'pytest'],
+        'test': ['coverage', 'pytest'],
     },
 
 
