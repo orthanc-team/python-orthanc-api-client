7aabcf506a8a1490e3f4058d2f5e4f8a958c8620
Alain Mazy <alain@mazy.be>
2022-02-08 12:01:51 +0100

    modalities.send

--- orthanc_api_client/modalities.py
+++ orthanc_api_client/modalities.py
@@ -18,3 +18,27 @@ class DicomModalities:
 
         if r.status_code == 200:
             return r.json()
+
+    def store(self, modality: str, resources_ids: Union[List[str], str], synchronous: bool = True):
+        """alias for send"""
+        return self.send(modality=modality, resources_ids=resources_ids, synchronous=synchronous)
+
+    def send(self, modality: str, resources_ids: Union[List[str], str], synchronous: bool = True):
+        """sends a list of resources to a remote DICOM modality
+
+        Returns
+        -------
+        Nothing, will raise if failing
+        """
+
+        if isinstance(resources_ids, str):
+            resources_ids = [resources_ids]
+
+        r = self._api_client.post(
+            relative_url=f"{self._url_segment}/{modality}/store",
+            json={
+                "Resources": resources_ids,
+                "Synchronous": synchronous
+            })
+
+        return None
