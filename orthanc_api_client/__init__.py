0cd9bcab55a024531ade1b8e276b745f9d9e74ce
Alain Mazy <alain@mazy.be>
2022-04-20 18:05:00 +0200

    fix

--- orthanc_api_client/__init__.py
+++ orthanc_api_client/__init__.py
@@ -2,6 +2,7 @@ from .api_client import OrthancApiClient
 from .exceptions import *
 from .helpers import generate_test_dicom_file
 from .change import ChangeType, ResourceType
+from .study import Study, StudyInfo
 
 # __all__ = [
 #     'OrthancApiClient'
