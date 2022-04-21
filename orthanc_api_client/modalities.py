5701fa5a77bd022f4d4245f808051854c46ca2e7
Alain Mazy <alain@mazy.be>
2022-04-21 12:48:09 +0200

    added move_study

--- orthanc_api_client/modalities.py
+++ orthanc_api_client/modalities.py
@@ -68,7 +68,7 @@ class DicomModalities:
 
         return None
 
-    def retrieve_study(self, from_modality: str, dicom_id: str, timeout: float = None) -> str:
+    def retrieve_study(self, from_modality: str, dicom_id: str) -> str:
         """
         retrieves a study from a remote modality (C-Move)
 
@@ -77,18 +77,42 @@ class DicomModalities:
         :param from_modality: the modality alias configured in orthanc
         :param dicom_id: the StudyInstanceUid of the study to retrieve
 
-        Returns: a Study instance of the study once it has been retrieved in orthanc
+        Returns: the study orthanc_id of the study once it has been retrieved in orthanc
         """
 
         # move the study from the remote modality to this orthanc
-        query = self._api_client.post(
-            relative_url=f"{self._url_segment}/{from_modality}/move",
-            json={'Level': 'Study',
-                  'Resources': [{'StudyInstanceUID': dicom_id}]
-                  })
+        self.move_study(
+            from_modality=from_modality,
+            dicom_id=dicom_id
+        )
 
         # this request has no real response '{}' if it succeeds
         return self._api_client.studies.find(dicom_id)
+
+    def move_study(self, from_modality: str, dicom_id: str, to_modality_aet: str = None):
+        """
+        moves a study from a remote modality (C-Move) to a target modality (AET)
+
+        this call is synchronous.  It completes once the C-Move is complete.
+
+        :param from_modality: the modality alias configured in orthanc
+        :param dicom_id: the StudyInstanceUid of the study to retrieve
+        :param to_modality_aet: the AET of the target modality
+        """
+
+        payload = {
+            'Level': 'Study',
+            'Resources': [{'StudyInstanceUID': dicom_id}]
+        }
+
+        if to_modality_aet:
+            payload['TargetAet'] = to_modality_aet
+
+        self._api_client.post(
+            relative_url=f"{self._url_segment}/{from_modality}/move",
+            json=payload)
+
+
 
     def query_studies(self, from_modality: str, query: object) -> typing.List[RemoteModalityStudy]:
         """
